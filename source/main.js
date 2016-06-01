f9d65aea835c8cbdab405409fa2650b3b9724391
Walter Bonetti <walter.bonetti@epitech.eu>
2016-06-01 23:55:51 +0200

    basic request and tokenizer

---
+++ source/main.js
@@ -0,0 +1,80 @@
+/**
+* @Author: Walter Bonetti <IniterWorker>
+* @Date:   2016-05-31T22:04:29+02:00
+* @Email:  walter.bonetti@epitech.eu
+* @Last modified by:   IniterWorker
+* @Last modified time: 2016-06-01T23:54:52+02:00
+* @License: MIT
+*/
+
+var crypto  = require('crypto');
+var request = require('request');
+
+function Blih(username, password)
+{
+  this.data = {
+    username: username,
+    token: '',
+    baseUrl: 'https://blih.epitech.eu/',
+  }
+  this.setPassword(password);
+}
+
+Blih.prototype.setPassword = function (password) {
+  var token = crypto.createHash('sha512');
+  token.update(password, 'utf8');
+  this.data.token = token.digest('hex');
+};
+
+Blih.prototype.getToken = function (password) {
+  return (this.data.token);
+};
+
+Blih.prototype.setBaseUrl = function (baseUrl) {
+  this.data.baseUrl = baseUrl;
+};
+
+Blih.prototype.sendRequest = function (signed_json, verb, path, callback) {
+
+	if (verb !== 'GET' && verb !== 'POST' && verb !== 'DELETE')
+		return (console.error('Error: Invalid http method'));
+
+	if (path.substring(0,12) !== 'repositories' && path.substring(0,6) !== 'sshkey')
+		return (console.error('Error: Invalid path data'));
+
+	var options = {
+		uri: this.data.baseUrl + "user/" + path,
+		method: verb,
+		json: signed_json
+	};
+
+	return (request(options, function (error, response, body) {
+		callback(body);
+	}));
+}
+
+Blih.prototype.createRequest = function (argData, postData, callback)
+{
+  var signatureData = {
+    user: "",
+    signature: ""
+  }
+
+  signatureData.user = this.data.username;
+  var hashing = crypto.createHmac('sha512', this.data.token);
+  hashing.update(this.data.username);
+  if (postData !== undefined)
+  {
+    var string = JSON.stringify(postData, null, 4);
+    hashing.update(string);
+    signatureData.data = postData;
+  }
+  signatureData.signature = hashing.digest('hex').toString();
+  return (this.sendRequest(signatureData, argData.verb, argData.path, callback));
+};
+
+Blih.prototype.print = function() {
+  console.log(this.data);
+};
+
+module.exports = Blih;
