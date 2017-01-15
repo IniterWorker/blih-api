579847d64f07cd3f11669d1a5d68ba33b604336b
Walter Bonetti <walter.bonetti@epitech.eu>
2017-01-15 22:17:22 +0100

    fix mistake

--- test/main.js
+++ test/main.js
@@ -1,11 +1,12 @@
 var blihApi = require('../source/main.js');
 var should = require('should');
-var blih = new blihApi();
+var blih = new blihApi("login_x", "password");
 
 
 describe('API', function () {
   it('Token is not empty', function() {
     var token = blih.generateToken("password");
+    console.log(token);
     token.should.not.undefined();
   });
 });
